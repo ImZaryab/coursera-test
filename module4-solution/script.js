05370dbc2bc52811535517e0cfb1cf26b9ab6f71
Zaryab <imzaryab@gmail.com>
2021-01-10 21:06:40 +0500

    Added Module4 Assignment Solution

---
+++ module4-solution/script.js
@@ -0,0 +1,16 @@
+(function (window) {
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var prop in names) {
+
+    var firstLetter = names[prop].charAt(0).toLowerCase();
+
+    if (firstLetter == 'j') {
+      byeSpeaker.speak(names[prop]);
+    } else {
+      helloSpeaker.speak(names[prop]);
+    }
+  }
+
+})(window);
